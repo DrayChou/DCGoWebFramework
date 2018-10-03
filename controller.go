fcb0face90738966e07a2ee6deb8a12a9cc206c7
Dray <hnzksq@gmail.com>
2018-10-04 01:08:53 +0800

    1. 添加多 app 支持 2. 封装 Mgo 3。 添加配置文件支持

--- controller.go
+++ controller.go
@@ -11,38 +11,38 @@ import (
 type Controller struct {
 	Response  http.ResponseWriter
 	Request   *http.Request
+	App       *application
 	SessionID string
 }
 
 func (p *Controller) SessionStart() {
-	p.SessionID = MySessionMgr.CheckCookieValid(p.Response, p.Request)
+	p.SessionID = p.App.SessionMgr.CheckCookieValid(p.Response, p.Request)
 	fmt.Println("CheckCookieValid:", p.SessionID)
 
 	if len(p.SessionID) < 1 {
 		//fmt.Println("StartSession:")
-		p.SessionID = MySessionMgr.StartSession(p.Response, p.Request)
+		p.SessionID = p.App.SessionMgr.StartSession(p.Response, p.Request)
 	}
 	fmt.Println("MySessionID:", p.SessionID)
 }
 
 func (p *Controller) SetSessionVal(key interface{}, value interface{}) {
-	MySessionMgr.SetSessionVal(p.SessionID, key, value)
+	p.App.SessionMgr.SetSessionVal(p.SessionID, key, value)
 }
 
 func (p *Controller) GetSessionVal(key interface{}) (interface{}, bool) {
-	return MySessionMgr.GetSessionVal(p.SessionID, key)
+	return p.App.SessionMgr.GetSessionVal(p.SessionID, key)
 }
 
 func (p Controller) Tpl(path string, data map[interface{}]interface{}) {
 	basePath, _ := os.Getwd()
 
-	tempPath := filepath.Join(basePath, "template", path+".html")
+	template_path := p.App.Config.Get("template_path").(string)
+	tempPath := filepath.Join(basePath, p.App.app, template_path, path+".html")
 	fmt.Println("tempPath:", tempPath)
 	if !IsPathExist(tempPath) {
 		return
 	}
-
-	fmt.Println("IsPathExist:", 1)
 
 	t, err := template.ParseFiles(tempPath)
 	if err != nil {
---
+++ example/app2/controller/index.go
@@ -0,0 +1,45 @@
+package controller
+
+import (
+	"../../.."
+	"encoding/json"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type IndexController struct {
+	DCGoWebFramework.Controller
+}
+
+type Person struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	Name      string
+	Phone     string
+	Timestamp time.Time
+}
+
+func (p IndexController) GET() {
+	mgodb, err1 := p.App.GetDB("main")
+	if err1 != nil {
+		panic(err1)
+	}
+
+	// Collection People
+	c := mgodb.(*mgo.Session).DB("test").C("people")
+	var results []Person
+
+	// Query All
+	err1 = c.Find(bson.M{"name": "Ale"}).Sort("-timestamp").All(&results)
+	if err1 != nil {
+		panic(err1)
+	}
+
+	b, err := json.Marshal(results)
+	if err != nil {
+		fmt.Fprintln(p.Response, "json.Marshal failed:", err)
+		return
+	}
+	fmt.Fprintln(p.Response, string(b))
+}
