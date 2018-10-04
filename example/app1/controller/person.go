0d4038b6601b3a60f9ee5b1de1c0fc62029c785c
Dray <hnzksq@gmail.com>
2018-10-05 01:04:26 +0800

    直接在 url 中解析链接地址

--- example/app1/controller/person.go
+++ example/app1/controller/person.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"fmt"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"strings"
 	"text/template"
 	"time"
 )
@@ -14,6 +16,9 @@ type PersonController struct {
 }
 
 func (p PersonController) Get() {
+	data := make(map[interface{}]interface{})
+	data["Title"] = "My Index page"
+
 	mgodb, err1 := p.App.GetDB("main")
 	if err1 != nil {
 		panic(err1)
@@ -22,27 +27,44 @@ func (p PersonController) Get() {
 	// Collection People
 	c := mgodb.(*mgo.Session).DB("test").C("people")
 
-	// Query All
-	var results []Person
-	err1 = c.Find(nil).Sort("-timestamp").All(&results)
+	items := make(map[interface{}]interface{})
 
+	var results []Person
+	fmt.Println(p.Request.URL)
+	pathArr := strings.Split(p.Request.RequestURI, "/")
+	if len(pathArr) == 3 {
+		id := string(pathArr[2])
+		// db.getCollection('people').find({"_id":ObjectId("5bb250d978422140d307809c")})
+		if strings.ToLower(id) != "index" {
+			fmt.Println(id)
+
+			// Query All
+			err1 = c.Find(bson.M{"_id": bson.ObjectIdHex(id)}).Sort("-timestamp").All(&results)
+			if err1 != nil {
+				panic(err1)
+			}
+
+			fmt.Println(results)
+			items[0] = results[0]
+
+			data["Items"] = items
+			p.Tpl("person-index", data)
+			return
+		}
+	}
+
+	// Query All
+	err1 = c.Find(nil).Sort("-timestamp").All(&results)
 	if err1 != nil {
 		panic(err1)
 	}
 
-	items := make(map[interface{}]interface{})
-	fmt.Println(results)
+	//fmt.Println(results)
 	for i := range results {
 		//fmt.Println(results[i])
 		items[i] = results[i]
 	}
-
-	data := make(map[interface{}]interface{})
-	data["Title"] = "My Index page"
 	data["Items"] = items
-
-	//fmt.Println(data)
-
 	p.Tpl("person-index", data)
 }
 
@@ -69,7 +91,7 @@ func (p PersonController) Post() {
 		panic(err1)
 	}
 
-	p.Response.Header().Set("Content-Type","text/html")
+	p.Response.Header().Set("Content-Type", "text/html")
 	var doc bytes.Buffer
 	var templateString = "ok, <a href='javascript:history.go(-1);'>return</a>"
 	t := template.New("fieldname example")
